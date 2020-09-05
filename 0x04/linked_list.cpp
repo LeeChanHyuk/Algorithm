5220b067e446e4a40c07a4682986c4b9a4702e48
chanhyuk <cjsgkcksgur@naver.com>
2020-09-05 21:51:07 +0900

    a

---
+++ 0x04/linked_list.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct NODE
+{
+    struct NODE
+    {
+        /* data */
+    };
+    
+    /* data */
+};
+
+
+int main()
+{
+
+}
