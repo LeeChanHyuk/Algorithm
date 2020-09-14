16bd9b19de8a90be1cc9e6fc928c1ff3871829d0
LeeChanHyuk <cjsgkcksgur@naver.com>
2020-09-14 21:20:12 +0900

    a

--- 0x09/2178.cpp
+++ 0x09/2178.cpp
@@ -8,26 +8,22 @@
 #define X second
 #define Y first
 using namespace std;
-int board[502][502];
+int via[502][502];
+string board[502];
 int main(void)
 {
-    int via[502][502];
     int dx[4] = {1, 0, -1, 0};
     int dy[4] = {0, 1, 0, -1};
     int width = 10;
     int height = 10;
-    scanf("%d %d", &height, &width); // \n을 만나기 전까지만 읽어오기 때문에 읽어오고나서 입력버퍼에 \n이 남아있다.
-    char a = getchar(); // 개행문자 처리용
-    char line[101];
+    int n, m;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> height >> width;
     for (int i = 0; i < height; i++)
-    {
-        fgets(line,101,stdin);
-        for (int j = 0; j < width; j++)
-        {
-            board[i][j] = (int)line[j] - 48;
-        }
-    }
-    int max = 99999;
+        cin >> board[i];
+    for (int i = 0; i < height; i++)
+        fill(via[i], via[i] + width, -1);
     queue<pair<int, int>> for_via_queue;
     via[0][0] = 1;
     for_via_queue.push({0, 0});
@@ -42,14 +38,12 @@ int main(void)
             int qy = y + dy[i];
             if (qx >= width || qy >= height || qx < 0 || qy < 0)
                 continue;
-            if (board[qy][qx] != 1)
-                continue;
-            if (via[qy][qx] >= 0) // 0이 아니라는 것-> 무언가 들어왔을 때, 원래꺼보다 더 크다면 더 돌아온 것.
+            if (board[qy][qx] != '1' || via[qy][qx] >= 0)
                 continue;
             via[qy][qx] = via[y][x] + 1; // 여기 넣지 않으면 여러번 중복되서 추가될 수도 있다.
             for_via_queue.push({qy, qx});
         }
     }
-    printf("%d", via[height-1][width-1]+1);
+    printf("%d", via[height - 1][width - 1]);
     return 0;
 }
