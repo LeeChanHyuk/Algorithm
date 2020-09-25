5ddcd82f3ee83b009b471ba0905e65c157ab073e
leechanhyuk <cjsgkcksgur@naver.com>
2020-09-25 18:57:09 +0900

    20200925 commit

---
+++ 0x0B/템플릿.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <string>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	return 0;
+}
