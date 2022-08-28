783cfba3485604e54f488984bfcdf2460b85278a
LeeChanHyuk <cjsgkcksgur@naver.com>
2022-08-28 17:11:02 +0900

    backtracking problem is all solved

--- review/1941.py
+++ review/1941.py
@@ -1,23 +1,56 @@
 import os
 from sys import stdin
-from turtle import back
-from xml.dom import minicompat
 
 board = []
 via = []
 for i in range(5):
-	board.append(stdin.readline().strip())
-	via.append([0] * 5)
+	input_list = stdin.readline().strip()
+	for j in range(5):
+		board.append(input_list[j])
+		via.append(0)
 
 horizontal_check = [0, 0, 0, 0, 0]
 vertical_check = [0, 0, 0, 0, 0]
 seven_princesses = []
+seven_princesses_positions = []
 result = 0
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
-def back_tracking(k):
+
+def bfs():
+	global seven_princesses_positions, via
+	# initialization for needy
+	queue = []
+	temp_via = via.copy()
+	temp_seven_princess_position = seven_princesses_positions.copy()
+
+	# bfs
+	first_coord = temp_seven_princess_position.pop()
+	queue.append(first_coord)
+	temp_via[first_coord[0] * 5 + first_coord[1]] = 1
+	student = 1
+	while len(queue) > 0:
+		origin_y, origin_x = queue.pop()
+		for i in range(4):
+			dest_y, dest_x = origin_y + dy[i], origin_x + dx[i]
+			if dest_y >= 5 or dest_y < 0 or dest_x >= 5 or dest_x < 0:
+				continue
+			if temp_via[dest_y * 5 + dest_x] == 1:
+				continue
+			if (dest_y, dest_x) not in temp_seven_princess_position:
+				continue
+			temp_via[dest_y * 5 + dest_x] = 1
+			queue.append((dest_y, dest_x))
+			student += 1
+	if student == 7:
+		return True
+	else:
+		return False
+
+
+def back_tracking(k, prev_i):
 	global seven_princesses, result
-	if k == 8:
+	if k == 7:
 		S = 0
 		Y = 0
 		for i in range(7):
@@ -26,34 +59,17 @@ def back_tracking(k):
 			else:
 				S += 1
 		if S >= 4:
-			print(seven_princesses)
-			result += 1
+			correct_seven_princess = bfs()
+			if correct_seven_princess:
+				result += 1
 	else:
-		for i in range(5):
-			for j in range(5):
-				if via[i][j] == 1:
-					continue
-				if k == 0:
-					via[i][j] = 1
-					seven_princesses.append(board[i][j])
-					back_tracking(k+1)
-					via[i][j] = 0
-					seven_princesses.pop()
-				success = 0
-				for l in range(4):
-					dest_i, dest_j = i + dy[l] , j + dx[l]
-					if dest_i < 0 or dest_i > 4 or dest_j < 0 or dest_j > 4:
-						continue
-					if via[dest_i][dest_j] == 1:
-						seven_princesses.append(board[i][j])
-						via[i][j] = 1
-						success = 1
-						break
-				if success:
-					back_tracking(k+1)
-					via[i][j] = 0
-					seven_princesses.pop()
-back_tracking(0)
+		for i in range(prev_i, 25):
+			seven_princesses.append(board[i])
+			seven_princesses_positions.append((int(i/5), i%5))
+			back_tracking(k+1, i+1)
+			seven_princesses.pop()
+			seven_princesses_positions.pop()
+back_tracking(0, 0)
 print(result)
 
 
